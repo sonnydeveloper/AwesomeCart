e807993c5cc636668d206be0d0db9bc4aab7fb49
Sanju Pandey <sanju.pandey@oroprise.com>
2020-05-05 22:52:35 +0530

    commit after creating multi slider

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
-# Create your views here.
+# Create your index for single slider.
+# def index(request):
+#     products = Product.objects.all()
+#     n = len(products)
+#     nSlides = n // 4 + ceil((n / 4) - (n // 4))
+#     params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+#     return render(request, 'shop/index.html', params)
+
 def index(request):
-    return render(request, 'shop/index.html')
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds': allProds}
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
