cd155850a8590e9424abf5ecc2fe2b8989e46f81
Sanju Pandey <sanju.pandey@oroprise.com>
2020-05-14 16:34:26 +0530

    commit after items checkout order in db

--- mac/shop/admin.py
+++ mac/shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Contact
-
 
 # Register your models here.
+from .models import Product, Contact, Orders
+
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 from math import ceil
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+# Create your views here.
+from django.http import HttpResponse
 
 
-# Create your index for single slider.
-# def index(request):
+def index(request):
+
 #     products = Product.objects.all()
 #     n = len(products)
 #     nSlides = n // 4 + ceil((n / 4) - (n // 4))
 #     params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
 #     return render(request, 'shop/index.html', params)
 
-def index(request):
     allProds = []
     catProds = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catProds}
@@ -57,4 +63,19 @@ def productView(request, myid):
 
 
 def checkout(request):
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
     return render(request, 'shop/checkout.html')
